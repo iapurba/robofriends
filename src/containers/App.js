51016fd238ec5feed4400b8295e59cad0abd09a1
Apurba Panja <apurbapanja1@gmail.com>
2020-12-06 11:38:59 +0530

    Implemented hooks in App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import CardList from '../components/CardList';
@@ -6,44 +6,37 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfiled] = useState('')
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({robots: users}));
+      .then(response => response.json())
+      .then(users => setRobots(users));
+  }, [])
+
+  const onSearchChange = (event) => {
+    setSearchfiled(event.target.value);
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value});
-  }
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
 
-  render () {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ?
-      <h1 className='tc'> Loading ... </h1> :
-      (
-        <div className='tc' >
-          <h1> Robo Friends </h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={ filteredRobots } />
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      );
-  }
+  return !robots.length ?
+    <h1 className='tc'> Loading ... </h1> :
+    (
+      <div className='tc' >
+        <h1> Robo Friends </h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
 }
 
 export default App;
